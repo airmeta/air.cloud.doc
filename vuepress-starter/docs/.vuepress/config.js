dbe8738a8d7c1cd9f0dd7ae7f807e71a8415431d
AC <2312932681@qq.com>
2024-08-12 18:59:48 +0800

    init

---
+++ vuepress-starter/docs/.vuepress/config.js
@@ -0,0 +1,63 @@
+
+module.exports = {
+    title: 'Air.Cloud.Core',
+    host: 'localhost',
+    description: 'Air.Cloud.Core Documentation',
+    themeConfig: {
+        logo: '/assets/logo.png',
+        sidebarDepth: 2,
+        displayAllHeaders: true,
+        activeHeaderLinks: true,
+        sidebar: {
+            '/guide/' : [
+                {
+                    title: '开始',
+                    collapsable: false,
+                    children: [
+                        '/guide/inject.md',
+                        '/guide/example.md',
+                        '/guide/use.md',
+                        '/guide/constractor.md',
+                    ]
+                },
+                {
+                    title: 'Air.Cloud.Core',
+                    collapsable: false,
+                    children: [
+                        '/guide/air-cloud-core/core.md',
+                        '/guide/air-cloud-core/libs.md',
+                        '/guide/air-cloud-core/plugins.md',
+                        '/guide/air-cloud-core/config.md',
+                    ]
+                },
+                {
+                    title: '模组',
+                    collapsable: false,
+                    children: [
+                        '/guide/air-cloud-core/modules/consul.md',
+                        '/guide/air-cloud-core/modules/kafka.md'
+                    ]
+                },
+                {
+                    title: '插件',
+                    collapsable: false,
+                    children: [
+                        '/guide/air-cloud-core/plugins/air_jwt.md',
+                        '/guide/air-cloud-core/plugins/air_swagger.md',
+                    ]
+                },
+                {
+                    title: '中间件',
+                    collapsable: false,
+                    children: [
+                        '/guide/middleware/consul.md',
+                        '/guide/middleware/kafka.md'
+                    ]
+                },
+               
+            ],
+            '/':[''], 
+        }
+      }
+  }
+
